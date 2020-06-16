21dd34d90b497ca466dbdc49db2296ea6ff7cdd5
Mateus Mello <mt.gatti@hotmail.com>
2020-06-16 15:16:57 -0300

    projeto funcionando

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Aula18Dojo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Jogador neymar = new Jogador();
+            neymar.Nome = "Neymar";
+
+            neymar.Peso = 85f;
+
+            neymar.Altura = 1.80f;
+
+            neymar.Posicao = "Atacante";
+
+            neymar.Nascimento = DateTime.Parse( "15/08/1989" );
+
+            
+            System.Console.WriteLine(neymar.CalcularIdade());
+            System.Console.WriteLine(neymar.VerificarAposentadoria());
+
+        }
+    }
+}
